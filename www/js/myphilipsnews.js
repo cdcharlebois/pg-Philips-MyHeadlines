05df8a14b579cf4623fbe3238e8a63d451c0a072
Conner Charlebois <cdcharlebois@gmail.com>
2018-01-08 10:19:29 -0500

    turn off saml

--- www/js/myphilipsnews.js
+++ www/js/myphilipsnews.js
@@ -1,7 +1,7 @@
 document.addEventListener("deviceready", function() {
 
     mxMobile.enableConnectionDetection();
-	mxMobile.enableSamlLogin();
+    // mxMobile.enableSamlLogin();
     mxMobile.overrideConnectionError();
     /*mxMobile.errorHandler(function(message) {
         if (message.indexOf("Unable to load") != -1) {
